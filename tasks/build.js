4958369594dfae542430e81c0c5bdcdb1b401374
Ryan Dy <rdy@pivotal.io>
2015-04-21 15:42:03 -0700

    Publish from dist folder instead of root
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
+var mergeStream = require('merge-stream');
 var plugins = require('gulp-load-plugins')();
-
 gulp.task('build', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(plugins.babel())
-    .pipe(gulp.dest('dist'));
+  return mergeStream(
+    gulp.src('src/**/*.js').pipe(plugins.babel()),
+    gulp.src(['LICENSE', 'README.md', 'package.json'])
+  ).pipe(gulp.dest('dist'));
 });
--- tasks/publish.js
+++ tasks/publish.js
@@ -3,15 +3,9 @@ var npm = require('npm');
 
 gulp.task('publish', ['build'], function(){
   npm.load({}, function(error) {
-    if (error) {
-      console.error(error);
-      return;
-    }
-
-    npm.commands.publish(['.'], function(error) {
-      if (error) {
-        console.error(error);
-      }
+    if (error) return console.error(error);
+    npm.commands.publish(['dist'], function(error) {
+      if (error) return console.error(error);
     });
   });
 });
