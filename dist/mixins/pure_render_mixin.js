96dfda6f3ca89c342e57177194d3d3aa2ba32c0f
Ryan Dy <rdy@pivotal.io>
2015-04-21 15:11:57 -0700

    build dist
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

---
+++ dist/mixins/pure_render_mixin.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Cursor = require('../cursor');
+
+function isNotEqual(next, current, ignore) {
+  return function (p) {
+    if (ignore.includes(p)) return false;
+    if (next[p] instanceof Cursor && current[p] instanceof Cursor) {
+      return !next[p].isEqual(current[p]);
+    }
+    return next[p] !== current[p];
+  };
+}
+
+var PureRenderMixin = {
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return [{ next: nextProps, current: this.props, type: 'Props' }, { next: nextState, current: this.state, type: 'State' }, { next: nextContext, current: this.context, type: 'Context' }].some(function (_ref) {
+      var next = _ref.next;
+      var current = _ref.current;
+      var type = _ref.type;
+
+      var ignore = 'ignorePureRender' + type;
+      return next && Object.keys(next).some(isNotEqual(next, current, this[ignore] || []));
+    }, this);
+  }
+};
+
+module.exports = PureRenderMixin;
